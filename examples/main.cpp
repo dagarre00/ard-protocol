62938d25098fd9ee2dd764d8cff98df1af66c908
Daniel Garcia <dagarre00@gmail.com>
2021-09-28 18:13:03 -0500

    Funciones integradas en parseDataTask

--- examples/main.cpp
+++ examples/main.cpp
@@ -15,21 +15,19 @@ const uint8_t key2 = 0xA6;
 
 bool parsed_data = false;
 
-Package dataParser(Datalink link, Stream &uart);
+Package dataParser(Datalink link, Stream& uart);
 
 static Package pack = dataParser(datalink, Serial);
 
 // Funciones del dispatcher:
 
-uint16_t funcion_prueba_1(uint16_t val)
-{
+uint16_t funcion_prueba_1(uint16_t val) {
     Serial.print("Funcion prueba 1 con valor: ");
     Serial.println(val);
     return 0;
 }
 
-uint16_t funcion_prueba_2(uint16_t val)
-{
+uint16_t funcion_prueba_2(uint16_t val) {
     Serial.print("Funcion prueba 2 con valor: ");
     Serial.println(val);
     return 0;
@@ -37,69 +35,43 @@ uint16_t funcion_prueba_2(uint16_t val)
 
 // Obtener paquete desde serial:
 
-Package dataParser(Datalink link, Stream &uart)
-{
+Package dataParser(Datalink link, Stream& uart) {
     link.read(uart);
     uint8_t size = link.available();
-    if (size)
-    {
+    if (size) {
         parsed_data = true;
         return Package(link.getPayload(), size);
     }
-    else
-    {
-        if (parsed_data) parsed_data = false; // evitar realizar escrituras en cada ciclo
+    else {
+        if (parsed_data)
+            parsed_data = false; // evitar realizar escrituras en cada ciclo
         return Package(0);
     }
 }
 
 //Funciones FreeRTOS (Multi task):
 
-void parseDataTask(void *parameter)
-{
-    TickType_t xLastWakeTime; // Variable necesaria para vTaskDelayUntil()
+void parseDataTask(void* parameter) {
+    TickType_t xLastWakeTime;            // Variable necesaria para vTaskDelayUntil()
     xLastWakeTime = xTaskGetTickCount(); // Funcion similar a millis()
-    while (true)
-    {
+    while (true) {
         vTaskDelayUntil(&xLastWakeTime, read_freq / portTICK_PERIOD_MS); // Similar a delay()
         pack = dataParser(datalink, Serial);
-    }
-}
-
-void checkForKeyTask(void *parameter)
-{
-    while (true)
-    {
         if (parsed_data) {
-            if (pack.hasValue(0x0A))
-            {
+            if (pack.hasValue(0x0A)) {
                 Package output = Package(3);
                 output.addData(0x0B, pack.getValue(0x0A));
                 datalink.send(output.dump(), output.getSize(), Serial);
             }
-        }
-        vTaskDelay(read_freq / portTICK_PERIOD_MS);
-    }
-}
-
-void dispatchTask(void *parameter)
-{
-    while (true)
-    {
-        if (parsed_data)
-        {
             dispatch.readPackage(pack.dump(), pack.getSize());
         }
-        vTaskDelay(read_freq / portTICK_PERIOD_MS);
     }
 }
 
-void sendDataTask(void *parameter)
-{
+void sendDataTask(void* parameter) {
     TickType_t xLastWakeTime;
-    xLastWakeTime = xTaskGetTickCount ();
-    while (true)
-    {
+    xLastWakeTime = xTaskGetTickCount();
+    while (true) {
         Package output = Package(3);
         output.addData(0xA4, 0);
         datalink.send(output.dump(), output.getSize(), Serial);
@@ -109,24 +81,19 @@ void sendDataTask(void *parameter)
 
 //Inicio Arduino:
 
-void setup()
-{
+void setup() {
     Serial.begin(115200);
     Serial.setTimeout(1000);
 
     dispatch.attachHandle(Handler(*funcion_prueba_1), key1);
     dispatch.attachHandle(Handler(*funcion_prueba_2), key2);
 
-    Serial.println("Attach handle OK");        
+    Serial.println("Attach handle OK");
 
-    xTaskCreate(parseDataTask, "Parse data periodically", 4096, (void *)&pack, 1, NULL);
-    xTaskCreate(checkForKeyTask, "Check for specific key", 4096, (void *)&pack, 1, NULL);
-    xTaskCreate(dispatchTask, "Process the Package through dispatcher", 4096, (void *)&pack, 1, NULL);
+    xTaskCreate(parseDataTask, "Parse data periodically", 4096, NULL, 1, NULL);
     xTaskCreate(sendDataTask, "Send data periodically", 4096, NULL, 1, NULL);
-
 }
 
-void loop()
-{
+void loop() {
     vTaskDelete(NULL);
 }
--- src/dispatcher/dispatcher.h
+++ src/dispatcher/dispatcher.h
@@ -4,30 +4,30 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-class Handler {
-    public:
-        Handler(uint16_t (*func)(uint16_t) = {});
-        uint16_t execute(uint16_t value_to_pass);
-    private: 
-        uint8_t key;
-        uint16_t (*func)(uint16_t);        
+class Handler
+{
+public:
+    Handler(uint16_t(*func)(uint16_t) = {});
+    uint16_t execute(uint16_t value_to_pass);
+private:
+    uint8_t key;
+    uint16_t(*func)(uint16_t);
 };
 
-class Dispatcher {
-    public:
-        Dispatcher(uint8_t buffer_size);
-        void attachHandle(Handler handle_user, uint8_t key);
-        void readPackage(uint8_t * package, uint8_t size);
-        uint8_t getHandleIndex(const uint8_t key);
-    private:
-        uint8_t size;
-        void handleKey(uint8_t key, uint16_t value);
-        void addHandle(uint8_t key, uint16_t (*func)(uint16_t));
-        uint8_t handler_index;
-        Handler * handle_list; 
-        uint8_t * added_keys;
+class Dispatcher
+{
+public:
+    Dispatcher(uint8_t buffer_size);
+    void attachHandle(Handler handle_user, uint8_t key);
+    void readPackage(uint8_t* package, uint8_t size);
+    uint8_t getHandleIndex(const uint8_t key);
+private:
+    uint8_t size;
+    void handleKey(uint8_t key, uint16_t value);
+    void addHandle(uint8_t key, uint16_t(*func)(uint16_t));
+    uint8_t handler_index;
+    Handler* handle_list;
+    uint8_t* added_keys;
 };
-
-
 
 #endif
