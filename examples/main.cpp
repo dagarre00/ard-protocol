bca00b4633aca9692398b1e12308122d86d4ffd6
Daniel Garcia <dagarre00@gmail.com>
2021-09-27 23:10:12 -0500

    Dispatcher funcionamiento inicial

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,15 +1,35 @@
 #include <Arduino.h>
 #include "datalink/datalink.h"
 #include "package/package.h"
-
+#include "dispatcher/dispatcher.h"
 
 Datalink datalink = Datalink(0x7E, 90);
 uint16_t send_freq = 3000;
+Dispatcher dispatch = Dispatcher(255);
+
 long timer = millis();
+
+const uint8_t key1 = 0xA4;
+const uint8_t key2 = 0xA6;
+
+uint16_t funcion_prueba_1 (uint16_t val) {
+    Serial.println("Funcion prueba 1");
+    return 0;
+}
+
+uint16_t funcion_prueba_2 (uint16_t val) {
+    Serial.println("Funcion prueba 2");
+    return 0;
+}
 
 void setup() {
     Serial.begin(115200);
     Serial.setTimeout(1000);
+
+    dispatch.attachHandle(Handler(*funcion_prueba_1),key1);
+    dispatch.attachHandle(Handler(*funcion_prueba_2),key2);
+
+    Serial.println("Attach handle OK");
 }
 
 Package dataParser(Datalink link, Stream &uart) {
@@ -30,7 +50,12 @@ void loop() {
         Package output = Package(3);
         output.addData(0x0B, pack.getValue(0x0A));
         datalink.send(output.dump(), output.getSize(), Serial);
+    } 
+
+    if (pack.getIndex() != 0){
+        dispatch.readPackage(pack.dump(), pack.getSize());
     }
+
 
     if (millis() - timer >= send_freq) {
         timer = millis();
@@ -38,5 +63,6 @@ void loop() {
         output.addData(0x0A, 10);
         datalink.send(output.dump(), output.getSize(), Serial);
     }
-
+    
+    delay(100);
 }
---
+++ src/dispatcher/dispatcher.cpp
@@ -0,0 +1,61 @@
+#include "dispatcher.h"
+
+Handler::Handler(uint16_t (*func)(uint16_t))
+{
+    this->func = func;
+}
+
+uint16_t Handler::execute(uint16_t value_to_pass)
+{
+    return this->func(value_to_pass);
+}
+
+Dispatcher::Dispatcher(uint8_t buffer_size)
+{
+    this->handler_index = 0;
+    this->added_keys = new uint8_t[buffer_size];
+    this->handle_list = new Handler[buffer_size];
+}
+
+void Dispatcher::attachHandle(Handler handle_user, uint8_t key)
+{
+    handle_list[handler_index] = handle_user;
+    added_keys[handler_index] = key;
+    this->handler_index++;
+}
+
+void Dispatcher::readPackage(uint8_t *package, uint8_t size)
+{
+    uint8_t local_key = 0;
+    uint16_t local_val = 0;
+    if (size % 3)
+    {
+        return;
+    }
+    for (int i = 0; i < size; i += 3)
+    {
+        local_key = package[i];
+        local_val = ((uint16_t)package[i + 2] << 8) | package[i + 1];
+        this->handleKey(local_key, local_val);
+    }
+}
+
+uint8_t Dispatcher::getHandleIndex(const uint8_t key)
+{
+    for (int i = 0; i < this->handler_index; i++)
+    {
+        if (key == this->added_keys[i])
+        {
+            return i;
+            break;
+        }
+    }
+    return -1;
+}
+
+void Dispatcher::handleKey(uint8_t key, uint16_t value)
+{
+    uint8_t handler_index = getHandleIndex(key);
+    Handler local_handler = handle_list[handler_index];
+    local_handler.execute(value);
+}
